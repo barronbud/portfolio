8934e36880bd00cf995de133bf04a6653f5265a1
Jeffrey Barron <me@jeffreybarron.dev>
2024-10-16 18:02:11 -0700

    Add recommendation panels

---
+++ app/components/recommendation.tsx
@@ -0,0 +1,65 @@
+import { SocialIcon } from "react-social-icons";
+
+interface Recommended {
+    content: string[];
+    name: string;
+    title: string;
+    link: string;
+}
+
+export default function Recommendation({
+    content,
+    name,
+    title,
+    link,
+}: Recommended) {
+    return (
+        <div className="flex items-start gap-2.5">
+            <div className="flex flex-col w-full leading-1.5 p-4 border-gray-200 bg-gray-100 rounded-e-xl rounded-es-xl dark:bg-gray-700">
+                <div className="w-4">
+                    <svg
+                        width="100%"
+                        height="100%"
+                        viewBox="0 0 28 20"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            fillRule="evenodd"
+                            clipRule="evenodd"
+                            d="M21.66 20C20.64 20 19.7 19.42 19.26 18.52L16.42 12.84C16.14 12.28 16 11.68 16 11.06V2C16 0.9 16.9 0 18 0H26C27.1 0 28 0.9 28 2V10C28 11.1 27.1 12 26 12H22L24.06 16.12C24.96 17.9 23.66 20 21.66 20ZM5.66 20C4.64 20 3.7 19.42 3.26 18.52L0.42 12.84C0.139999 12.28 0 11.68 0 11.06V2C0 0.9 0.9 0 2 0H10C11.1 0 12 0.9 12 2V10C12 11.1 11.1 12 10 12H6L8.06 16.12C8.96 17.9 7.66 20 5.66 20Z"
+                            fill="currentColor"
+                        ></path>
+                    </svg>
+                </div>
+                {content.map((para, i) => (
+                    <p
+                        className="font-normal py-2.5 text-gray-900 dark:text-white"
+                        key={`paragraph${i}`}
+                    >
+                        {para}
+                    </p>
+                ))}
+
+                <div className="flex flex-col">
+                    <span className="font-semibold text-gray-900 dark:text-white">
+                        {name}
+                        <SocialIcon
+                            url={link}
+                            target="_blank"
+                            style={{
+                                width: 20,
+                                height: 20,
+                                marginLeft: 5,
+                            }}
+                        />
+                    </span>
+
+                    <span className="text-sm font-normal text-gray-500 dark:text-gray-400">
+                        {title}
+                    </span>
+                </div>
+            </div>
+        </div>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,42 @@
 import { BlogPosts, BlogPostsSneakPeek } from "app/components/posts";
 import { SocialIcon } from "react-social-icons";
 import Image from "next/image";
+import { ArrowIcon } from "./components/footer";
+import Recommendation from "./components/recommendation";
+import { title } from "process";
+
+const recommendations = [
+    {
+        name: "Anthony Bull",
+        title: "Director of Engineering @ Rad AI",
+        link: "https://www.linkedin.com/in/anthony-bull",
+        content: [
+            "I had the pleasure of working with Jeffrey when I hired him as CourseKey's first (ever) Engineering Manager.",
+            "He played a crucial role in our successful transition from a flat structure to a pod-based model. His ability to inspire and guide during that ambiguous, somewhat-chaotic time, as well as his keen understanding of the technical aspects of the work, made him an invaluable asset during that period of change.",
+        ],
+    },
+    {
+        name: "Brian Sweatt",
+        title: "CTO @ BitCoin Depot",
+        link: "https://www.linkedin.com/in/bsweatt",
+        content: [
+            "Given the opportunity I would jump at the chance to work with Jeff again, in any capacity. He is an engineering leader I trust completely to be empathetic, build a great team, culture, and product, as well as get the most out of the people he works with, myself included.",
+        ],
+    },
+    {
+        name: "Tiffany Lottering",
+        title: "Principal Product Designer @ Routable (YC '17)",
+        link: "https://www.linkedin.com/in/tiffany-lottering-06750a1a",
+        content: [
+            "Jeffrey has this unique combo of tech expertise and strategic thinking that made our collaboration between engineering and design seamless. He created an inclusive work environment where we freely shared ideas and worked together to deliver innovative, user-centered solutions.",
+        ],
+    },
+];
 
 export default function Page() {
     return (
         <section>
-            <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+            <h1 className="mb-8 text-3xl font-semibold tracking-tighter">
                 About Me
             </h1>
             <div className="flex">
@@ -16,6 +47,7 @@ export default function Page() {
                         height={1015}
                         alt="Jeffrey Barron profile picture"
                         className="rounded-3xl"
+                        priority
                     />
                     <div className="my-4">
                         <SocialIcon url="https://www.linkedin.com/in/jeffreyabarron/" />
@@ -23,7 +55,7 @@ export default function Page() {
                     </div>
                 </div>
                 <div className="mx-4">
-                    <h2 className="text-4xl font-bold py-4">Jeffrey Barron</h2>
+                    <h2 className="text-2xl font-bold py-4">Jeffrey Barron</h2>
                     <ul>
                         <li>
                             10+ years in software development across fin-tech,
@@ -34,21 +66,36 @@ export default function Page() {
                             more remote full-stack teams
                         </li>
                         <li>
-                            Developed and led teams using Node, React, Python,
-                            and Typescript
+                            Develop and lead teams utilizing Node, React,
+                            Python, and Typescript
                         </li>
                         <li>
-                            Led Agile transformations by applying Scrum, Kanban,
-                            and other methodologies
+                            Lead Agile transformations by applying Scrum,
+                            Kanban, and other methodologies
                         </li>
                         <li>
-                            Hired and grew empowered cross-functional teams with
+                            Hire and grow empowered cross-functional teams with
                             a high bias towards action
                         </li>
                     </ul>
                 </div>
             </div>
-            <h2 className="my-8 text-xl font-semibold tracking-tighter">
+            <h2 className="mb-8 text-3xl font-semibold tracking-tighter">
+                Recommendations
+            </h2>
+            <div className="grid grid-cols-1 md:grid-cols-3">
+                {recommendations.map((recommendation) => (
+                    <div className="mb-4 md:mr-4 p-2" key={recommendation.name}>
+                        <Recommendation
+                            name={recommendation.name}
+                            title={recommendation.title}
+                            link={recommendation.link}
+                            content={recommendation.content}
+                        />
+                    </div>
+                ))}
+            </div>
+            <h2 className="my-8 text-3xl font-semibold tracking-tighter">
                 Recent Articles
             </h2>
             <div className="my-8 card-preview">
