330758b169dc4252da8bfa670a665a252446730b
Jeffrey Barron <me@jeffreybarron.dev>
2024-10-18 21:55:26 -0700

    Add rudimentary layout and nav for OMS demo app

---
+++ oms/app/components/nav.tsx
@@ -0,0 +1,16 @@
+export function Navbar() {
+    return (
+        <aside className="-ml-[8px] mb-16 tracking-tight">
+            <div className="lg:sticky lg:top-20">
+                <nav
+                    className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
+                    id="nav"
+                >
+                    <div className="flex flex-row space-x-0 pr-10">
+                        <a href="/demos">&lt; Back to demos</a>
+                    </div>
+                </nav>
+            </div>
+        </aside>
+    );
+}
