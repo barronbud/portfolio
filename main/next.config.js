dbf45f8a4dd6c40c69348cf5f857415417e076a9
Jeffrey Barron <me@jeffreybarron.dev>
2024-10-18 21:39:15 -0700

    Tweak demo asset rewrite

--- main/next.config.js
+++ main/next.config.js
@@ -2,16 +2,16 @@ const nextConfig = {
     async rewrites() {
         return [
             {
+                source: "/demos/_next/:path+",
+                destination: `${process.env.OMS_DOMAIN}/_next/:path+`,
+            },
+            {
                 source: "/demos/order-management-system",
                 destination: `${process.env.OMS_DOMAIN}/`,
             },
             {
                 source: "/demos/order-management-system/:path+",
                 destination: `${process.env.OMS_DOMAIN}/:path+`,
-            },
-            {
-                source: "/demos/_next/:path+",
-                destination: `${process.env.OMS_DOMAIN}/demos/_next/:path+`,
             },
         ];
     },
