78ece0035395a1414e44fbcd21a06a8c0c9fd8ae
Jeffrey Barron <me@jeffreybarron.dev>
2024-10-23 20:02:57 -0700

    Adjust carousel speed down a bit

--- main/app/components/carousel.tsx
+++ main/app/components/carousel.tsx
@@ -6,13 +6,13 @@ import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/outline";
 export default function Carousel({ items, title }) {
     const [currentIndex, setCurrentIndex] = useState(0);
 
-    const goToPrevious = () => {
+    const previous = () => {
         setCurrentIndex((prevIndex) =>
             prevIndex === 0 ? items.length - 1 : prevIndex - 1
         );
     };
 
-    const goToNext = () => {
+    const next = () => {
         setCurrentIndex((prevIndex) =>
             prevIndex === items.length - 1 ? 0 : prevIndex + 1
         );
@@ -20,22 +20,22 @@ export default function Carousel({ items, title }) {
 
     useEffect(() => {
         setTimeout(() => {
-            goToNext();
-        }, 10000);
+            next();
+        }, 8000);
     }, [currentIndex]);
 
     return (
         <div className="flex items-center justify-center mb-8 z-10 relative">
             <button
                 className="p-4 mr-6 bg-blue-500 text-white rounded-lg"
-                onClick={goToPrevious}
+                onClick={previous}
             >
                 <ChevronLeftIcon className="w-6 h-6" />
             </button>
             <div className="text-center w-full">{items[currentIndex]}</div>
             <button
                 className="p-4 ml-6 bg-blue-500 text-white rounded-lg"
-                onClick={goToNext}
+                onClick={next}
             >
                 <ChevronRightIcon className="w-6 h-6" />
             </button>
