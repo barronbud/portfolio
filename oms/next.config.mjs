f386581573116734b86de8b1a6d1859c74e2965e
Jeffrey Barron <me@jeffreybarron.dev>
2024-11-03 15:15:16 -0800

    Add allowed origin to OMS app

--- oms/next.config.mjs
+++ oms/next.config.mjs
@@ -3,6 +3,11 @@ import { withVercelToolbar } from "@vercel/toolbar/plugins/next";
 
 const nextConfig = {
     assetPrefix: "/demos",
+    experimental: {
+        serverActions: {
+            allowedOrigins: [`${process.env.MAIN_ORIGIN}`],
+        },
+    },
 };
 
 export default withVercelToolbar()(nextConfig);
