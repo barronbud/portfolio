a4cc9a8db9578c1f0d37800207641ccd418d0dc9
Jeffrey Barron <me@jeffreybarron.dev>
2025-04-27 12:15:27 -0700

    Remove all demo-related code and references

--- oms/app/orders/create/page.tsx
+++
@@ -1,9 +0,0 @@
-import OrderForm from "@/components/orders/form";
-
-export default function CreateOrderPage() {
-    return (
-        <div>
-            <OrderForm />
-        </div>
-    );
-}
--- oms/flags.ts
+++
@@ -1,3 +0,0 @@
-import { flag } from "@vercel/flags/next";
-
-export const showFullOMS = flag({ key: "showFullOMS", decide: () => false });
