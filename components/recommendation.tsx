7b70e1bbacd3cf3aa5b03b23c45fe7845707d2d6
Jeffrey Barron <me@jeffreybarron.dev>
2025-12-01 19:44:28 -0800

    Fix line height for recommendations

--- components/recommendation.tsx
+++ components/recommendation.tsx
@@ -8,45 +8,32 @@ interface Recommended {
     link: string;
 }
 
-export default function Recommendation({
-    content,
-    name,
-    title,
-    link,
-}: Recommended) {
+export default function Recommendation({ content, name, title, link }: Recommended) {
     return (
-        <div className="flex items-start gap-2.5">
-            <div className="flex flex-col w-full leading-1.5 p-4 border-gray-500 border rounded-e-xl rounded-es-xl">
-                <div className="w-4">
-                    <MessageSquareQuote className="w-6 h-6 text-gray-400" />
-                </div>
-                {content.map((para, i) => (
-                    <p
-                        className="font-normal py-2.5 text-gray-900 dark:text-white"
-                        key={`paragraph${i}`}
-                    >
-                        {para}
-                    </p>
-                ))}
+        <div className="flex flex-col w-full p-4">
+            <div className="mb-4">
+                <MessageSquareQuote className="w-6 h-6 text-gray-400 dark:text-gray-500" />
+            </div>
+            {content.map((para, i) => (
+                <p className="font-normal py-2.5 text-gray-900 dark:text-white leading-relaxed" key={`paragraph${i}`}>
+                    {para}
+                </p>
+            ))}
 
-                <div className="flex flex-col">
-                    <span className="font-semibold text-gray-900 dark:text-white">
-                        {name}
-                        <SocialIcon
-                            url={link}
-                            target="_blank"
-                            style={{
-                                width: 20,
-                                height: 20,
-                                marginLeft: 5,
-                            }}
-                        />
-                    </span>
+            <div className="flex flex-col mt-4">
+                <span className="font-semibold text-gray-900 dark:text-white flex items-center gap-2">
+                    {name}
+                    <SocialIcon
+                        url={link}
+                        target="_blank"
+                        style={{
+                            width: 20,
+                            height: 20,
+                        }}
+                    />
+                </span>
 
-                    <span className="text-sm font-normal text-gray-500 dark:text-gray-400">
-                        {title}
-                    </span>
-                </div>
+                <span className="text-sm font-normal text-gray-500 dark:text-gray-400">{title}</span>
             </div>
         </div>
     );
