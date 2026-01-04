92931df02055c27ccaaab0392732f63d7e2d2b14
Jeffrey Barron <me@jeffreybarron.dev>
2026-01-03 18:33:42 -0800

    Bump all package versions

--- components/mdx.tsx
+++ components/mdx.tsx
@@ -5,9 +5,7 @@ import { highlight } from "sugar-high";
 import React from "react";
 
 function Table({ data }: { data: { headers: string[]; rows: string[][] } }) {
-    const headers = data.headers.map((header, index) => (
-        <th key={index}>{header}</th>
-    ));
+    const headers = data.headers.map((header, index) => <th key={index}>{header}</th>);
     const rows = data.rows.map((row, index) => (
         <tr key={index}>
             {row.map((cell, cellIndex) => (
@@ -40,21 +38,8 @@ function CustomLink(props: { href: string; children: React.ReactNode }) {
     return <a target="_blank" rel="noopener noreferrer" {...props} />;
 }
 
-function RoundedImage(props: {
-    alt: string;
-    src: string;
-    width: number;
-    height: number;
-}) {
-    return (
-        <Image
-            className="rounded-lg"
-            alt={props.alt}
-            src={props.src}
-            width={props.width}
-            height={props.height}
-        />
-    );
+function RoundedImage(props: { alt: string; src: string; width: number; height: number }) {
+    return <Image className="rounded-lg" alt={props.alt} src={props.src} width={props.width} height={props.height} />;
 }
 
 function Code({ children, ...props }: { children: string }) {
@@ -74,18 +59,17 @@ function slugify(str: string) {
 }
 
 function createHeading(level: number) {
-    const Heading = ({ children }: { children: string }) => {
-        const slug = slugify(children);
+    const Heading = ({ children }: { children: React.ReactNode }) => {
+        const childrenText = typeof children === "string" ? children : String(children);
+        const slug = slugify(childrenText);
         return React.createElement(
             `h${level}`,
             { id: slug },
-            [
-                React.createElement("a", {
-                    href: `#${slug}`,
-                    key: `link-${slug}`,
-                    className: "anchor",
-                }),
-            ],
+            React.createElement("a", {
+                href: `#${slug}`,
+                key: `link-${slug}`,
+                className: "anchor",
+            }),
             children
         );
     };
@@ -109,10 +93,5 @@ const components = {
 };
 
 export function CustomMDX(props: { source: string; components?: any }) {
-    return (
-        <MDXRemote
-            {...props}
-            components={{ ...components, ...(props.components || {}) }}
-        />
-    );
+    return <MDXRemote {...props} components={{ ...components, ...(props.components || {}) }} />;
 }
